8e8a6ce42b2e300149efb82a5c268fb540a89c04
Maksym ZUBRYTSKYI <maksym.zubrytskyi-ext@ledger.fr>
2024-06-21 12:51:53 +0300

    testing stg tezos node v20-1-2

--- libs/env/src/env.ts
+++ libs/env/src/env.ts
@@ -163,7 +163,7 @@ const envDefinitions = {
     desc: "tzkt.io explorer",
   },
   API_TEZOS_NODE: {
-    def: "https://xtz-node.api.live.ledger.com",
+    def: "https://xtz-node.api-01.live.ledger-stg.com",
     parser: stringParser,
     desc: "node API for tezos (for broadcast only)",
   },
