d7ae85f5167cf4bb7256134c70a87bd1b03efe70
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-08-20 17:09:53 +0200

    cleanup and fix hw-getAddress

--- libs/coin-modules/coin-cosmos/src/hw-getAddress.ts
+++ libs/coin-modules/coin-cosmos/src/hw-getAddress.ts
@@ -1,25 +1,6 @@
-// import Cosmos from "@ledgerhq/hw-app-cosmos";
-// import type { Resolver } from "../../hw/getAddress/types";
-// import cryptoFactory from "../chain/chain";
-
-// const resolver: Resolver = async (transport, { path, verify, currency }) => {
-//   const cosmosApiImpl = cryptoFactory(currency.id);
-//   const cosmos = new Cosmos(transport);
-//   const r = await cosmos.getAddress(path, cosmosApiImpl.prefix, verify || false);
-//   return {
-//     address: r.address,
-//     publicKey: r.publicKey,
-//     path,
-//   };
-// };
-
-// export default resolver;
-
 import { GetAddressFn } from "@ledgerhq/coin-framework/bridge/getAddressWrapper";
 import { SignerContext } from "@ledgerhq/coin-framework/signer";
 import { GetAddressOptions } from "@ledgerhq/coin-framework/derivation";
-// import { StrKey } from "@stellar/stellar-sdk";
-// import { StellarSigner } from "./types/signer";
 import { CosmosSigner } from "./types";
 import cryptoFactory from "./chain/chain";
 
@@ -27,16 +8,15 @@ function resolver(signerContext: SignerContext<CosmosSigner>): GetAddressFn {
   return async (deviceId: string, { path, verify, currency }: GetAddressOptions) => {
       const cosmosApiImpl = cryptoFactory(currency.id);
 
-    const publicKey = await signerContext(deviceId, async signer => {
+    const {address, publicKey} = await signerContext(deviceId, async signer => {
+      // TODO: recheck param
       const { address, publicKey } = await signer.getAddress(path, parseInt(cosmosApiImpl.prefix), verify || false);
-      return publicKey;
+      return { address, publicKey};
     });
-    // const publicKey = StrKey.encodeEd25519PublicKey(rawPublicKey);
-
     return {
+      address: address,
+      publicKey: publicKey, //publicKey.toString("hex"),
       path,
-      address: publicKey,
-      publicKey: publicKey.toString("hex"),
     };
   };
 }
